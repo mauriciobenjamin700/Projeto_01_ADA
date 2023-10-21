79390209690a9e618ec10e04a146b13a9ddf928c
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2023-10-21 12:58:37 -0300

    Preparando a estrutura do sistema
    Criação dos principais arquivos que serão usados no sistema.

---
+++ func.py
@@ -0,0 +1,44 @@
+##################### Restaurant func ###############
+def add_restaurant():
+    pass
+
+def modify_restaurant():
+    pass
+
+def remove_restaurant():
+    pass
+
+def show_menu(restaurant):
+    pass
+    
+
+
+#################### Menu func ##############
+
+def add_item():
+    pass
+
+def edit_item():
+    pass
+
+def remove_item():
+    pass
+
+################## show info ################
+
+def show_list_restaurant():
+    pass
+
+def show_describ_all_restaurant():
+    pass
+
+################ user interface #######
+
+def interface():
+    opc = 0
+    print("\nBem vindo ao nosso sistema")
+    
+    opc = int(input("Você pode acessar as seguintes funcionalidades: \n\t1 - F1\n\t2 - F2\n\t1 - F3\n\tSua escolha: "))
+    
+    
+    return opc
---
+++ main.py
@@ -0,0 +1,8 @@
+restaurants = []
+from func import *
+
+opc = 1
+while opc != 0 :
+    opc = interface()
+    
+    
